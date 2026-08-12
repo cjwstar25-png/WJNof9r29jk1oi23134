6a3bcec44480791796991dc40af210739e83721f
cjwstar25-png <cjwstar25@gmail.com>
2026-08-12 14:14:02 +0900

    ㅈ

--- 1184FIWKMmnmWNyIK5M.lua
+++ 1184FIWKMmnmWNyIK5M.lua
@@ -1,4 +1,4 @@
-loadstring(game:HttpGet(""))()
+loadstring(game:HttpGet("https://raw.githubusercontent.com/cjwstar25-png/WJNof9r29jk1oi23134/refs/heads/main/Cubic.lua"))()
 
 --!nocheck
 ----------------------------------------------------------------
